ccaf5a188ab0bb077a55c6690e517fe6c44f6492
Rajeshkumar Ganesan <rajeskumarcse@gmail.com>
2025-08-15 11:36:14 -0500

    add deps

--- varavu_selavu_ui/src/App.tsx
+++ varavu_selavu_ui/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { JSX } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router, Route, Routes, useNavigate, Navigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
